bb27c67da70a05e80fe03e5fed3c6d589ba780f1
CrazyLazy <crazylazy007@gmail.com>
2017-06-12 23:59:12 +0800

    5.02

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -2,7 +2,7 @@ var dataArray = [5,11,18];
 
 var svg = d3.select('body').append('svg')
                              .attr("width","100%")
-                             .attr("height","300%");
+                             .attr("height","100vh");
 
 svg.selectAll('rect')
    .data(dataArray)
@@ -21,3 +21,23 @@ svg.selectAll('circle')
              .attr("fill","#666")
              .attr("cx",function(d,i){newX = newX+d*10;return newX;})
              .attr("cy",function(d,i){return 150;});
+
+var newX = 900;
+svg.selectAll('line')
+  .data(dataArray)
+  .enter().append('line')
+            .attr("x1",newX)
+            .attr("x2",function(d,i){return newX+d*15;})
+            .attr("y1",function(d,i){return 150+i*30;})
+            .attr("y2",function(d,i){return 150+i*30;})
+            .attr("stroke-width","4");
+
+var textArray = ['start','middle','end'];
+svg.append('text').selectAll('tspan')
+  .data(textArray)
+  .enter().append('tspan')
+    .attr('x',newX)
+    .attr('y',function(d,i){return 220+i*30;})
+    .attr('text-anchor',function(d,i){return d;})
+    .attr('font-size','30')
+    .text(function(d,i){return d;});
