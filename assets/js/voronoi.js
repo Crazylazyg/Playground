d2d91790faea6fca041537d9e6b9d631b5df1a62
CrazyLazy <crazylazy007@gmail.com>
2017-06-15 18:02:32 +0800

    hover
    https://bl.ocks.org/mbostock/ec10387f24c1fad2acac3bc11eb218a5

--- assets/js/voronoi.js
+++ assets/js/voronoi.js
@@ -1,17 +1,26 @@
-var width = 960;
-var height = 500;
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var toolTip = d3.select('body').append('div').attr('id','toolTip');
 
 var vertices = d3.range(100).map(function(d){return [Math.random()*width,Math.random()*height];});
 
-var voronoi = d3.voronoi().size([width,height]);
-var svg = d3.select('body').append('svg').attr('width','100%').attr('height','100vh');
+var voronoi = d3.voronoi().size([width,height]).extent([[1,1],[width-1,height-1]]);
+var svg = d3.select('body').append('svg').attr('width','100vw').attr('height','100vh');
 
 svg.append('g').attr('class','polygons').selectAll('path')
   .data(voronoi.polygons(vertices))
   .enter().append('path')
     .attr('d',function(d){
       return 'M'+d.join('L')+'Z';
+    })
+    .on('mousemove',function(d){
+      toolTip.style("opacity",'1')
+      .style('left',d3.event.pageX+6+'px')
+      .style('top',d3.event.pageY+10+'px');
+      toolTip.html("Number of sides: "+d.length);
     });
+
 
 svg.append('g').attr('class','links').selectAll('path')
   .data(voronoi.links(vertices))
