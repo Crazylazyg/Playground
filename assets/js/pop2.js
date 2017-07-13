075d205a5a19c304a7151988f2e0c74941c5dd80
CrazyLazy <crazylazy007@gmail.com>
2017-07-13 17:58:07 +0800

    bar
    可调整时间

--- assets/js/pop2.js
+++ assets/js/pop2.js
@@ -6,16 +6,22 @@ var margin = {
       right: 0,
     },
     width = window.innerWidth - margin.left - margin.right - 100,
-    height = Math.min(500, window.innerHeight-margin.top-margin.bottom-10);
+    height = Math.min(560, window.innerHeight-margin.top-margin.bottom-10);
+
+//countryTip
+var countryTip = d3.select('#wrap').append('div').attr('id','countryTip').html('The Wealth & Health of Nations');
+
 var c20c = d3.scaleSequential(d3.interpolateMagma).domain([0,210]);
+
 var svg = d3.select('#wrap').append('svg')
       .attr('width',width+margin.left+margin.right)
       .attr('height',height+margin.top+margin.bottom)
       .attr('transform','translate('+ (-margin.left)+','+margin.top+')');
+
 var textYear = svg.append('text')
   .attr("class",'textYear')
-  .attr('text-anchor','middle')
-  .attr('transform','translate('+ width/2 +','+(height/2 + 40)+')'),
+  .attr('text-anchor','end')
+  .attr('transform','translate('+ width +','+(height - 60)+')'),
   yText = svg.append('text')
     .attr('class','tipText')
     .attr('transform',"translate("+20+","+20+")")
@@ -24,7 +30,7 @@ var textYear = svg.append('text')
     .attr('class','tipText')
     .attr('text-anchor','end')
     .attr('transform','translate('+ width +','+ (height-20) +')')
-    .html('GDP/$');
+    .html('GDP Income/person');
 
 var nameDic;
 d3.csv('assets/js/data/geoName.csv').get(function(error, data){
@@ -94,12 +100,14 @@ function draw(dataLife,dataCapita,dataPop) {
       popMax = d3.max(dataPop, function(d){return d.population}),
       popMin = d3.min(dataPop, function(d){return d.population});
 
-  var x = d3.scalePow().exponent(0.3333)
+  var x = d3.scalePow().exponent(0.333)
           .domain([0,capMax])
-          .range([0,width]),
-      y = d3.scaleLinear()
+          .range([0,width])
+          .clamp(true),
+      y = d3.scalePow().exponent(1.2  )
           .domain([lifeMin,lifeMax])
-          .range([height,0]),
+          .range([height,0])
+          .clamp(true),
       r = d3.scalePow().exponent(0.5)
           .domain([popMin,popMax])
           .range([1,72]);
@@ -118,10 +126,34 @@ function draw(dataLife,dataCapita,dataPop) {
   //     })
   //     .entries(dataLife);
   var yAxis = d3.axisLeft(y),
-      xAxis = d3.axisBottom(x).tickArguments([6, "s"]);
+      xAxis = d3.axisBottom(x).scale(x).tickArguments([6, "s"]);
 
   svg.append('g').attr('class','axis y').call(yAxis);
   svg.append('g').attr('class','axis x').attr('transform','translate(0,'+height+')').call(xAxis);
+  var barHeight = 32;
+  var yearSelector = svg.append('rect')
+        .attr('class','selector')
+        .attr('x',0)
+        .attr('y', height + 40)
+        .attr("width", width)
+        .attr("height", barHeight);
+
+  var yearBar = svg.append('rect')
+        .attr('class', 'bar')
+        .attr('x', 0)
+        .attr('y', height + 40)
+        .attr("height", barHeight);
+
+  var overlay = svg.append('rect')
+        .attr('class','overlay')
+        .attr('x',0)
+        .attr('y', height + 40)
+        .attr("width", width)
+        .attr("height", barHeight)
+        .on("mouseover", enableInteraction);
+
+
+
   // cleaing data
   var dataAll = dataLife.concat(dataCapita).concat(dataPop);
   var nested = d3.nest()
@@ -154,10 +186,11 @@ function draw(dataLife,dataCapita,dataPop) {
     // debugger;
     var dataG = filtered[0].values;
     var country = svg.selectAll('circle');
+    setTimeout(textYear.html(year), 500);
     if (start) {
       country.data(dataG,function(d){return d.key;})
         .enter().append('circle')
-          .attr('class',function(d){return d.key + ' host'})
+          .attr('class', 'host')
           .attr('fill', function(d,i){ return c20c(i);})
           .attr("cx",function(d){
             // debugger;
@@ -185,12 +218,23 @@ function draw(dataLife,dataCapita,dataPop) {
            }else{
              return 0;
            }
+          }).on('mousemove', function(d){
+            var name;
+            for (i in nameDic) {
+              // debugger;
+              if (nameDic[i].geo == d.key) {
+                name = nameDic[i].name
+              }
+            }
+            countryTip
+              .html(name);
           });
+
       start = false;
     }else{
-      country.data(dataG,function(d){return d.key;})
-        .attr('class',function(d){return d.key + ' host'})
+      country.data(dataG, function(d){return d.key;})
         .transition()
+        .ease(d3.easeLinear)
           .attr("cx",function(d){
             // debugger;
             if(d.value){
@@ -224,14 +268,56 @@ function draw(dataLife,dataCapita,dataPop) {
 
   }
   // debugger;
+  var widthScale = d3.scaleLinear()
+      .domain([1951, 2016])
+      .range([0, width]);
 
-  var yearIndex = 1950;
+  var yearIndex = 1951;
   var yearInterval = setInterval(function(){
     update(yearIndex);
-    textYear.html(yearIndex);
+    yearBar
+    .transition()
+    .ease(d3.easeLinear)
+      .attr('width', widthScale(yearIndex));
     yearIndex++;
     if (yearIndex > 2016){
       clearInterval(yearInterval);
     }
   }, 500);
+
+
+  function enableInteraction(){
+      var yearScale = d3.scaleLinear()
+          .domain([0, width])
+          .range([1951, 2016])
+          .clamp(true);
+      // debugger;
+      // Cancel the current transition, if any.
+      // country.transition().duration(0);
+
+      overlay
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout)
+          .on("mousemove", mousemove)
+          .on("touchmove", mousemove);
+
+      function mouseover() {
+        // setStart(true);
+      }
+
+      function mouseout() {
+        // setStart(Math.round(yearScale(d3.mouse(this)[0])));
+      }
+
+      function mousemove() {
+        clearInterval(yearInterval);
+        var width = widthScale(yearScale(d3.mouse(this)[0]));
+
+        yearBar
+          .transition().duration(60)
+          .attr('width', width);
+        // console.log(yearScale(d3.mouse(this)[0]));
+        update(Math.round(yearScale(d3.mouse(this)[0])));
+      }
+  }
 }
